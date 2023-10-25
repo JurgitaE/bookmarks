775f7a64d4af678d3b8439c6fea02e3b6e800509
Jurgita <jurgita.eicinaite@yahoo.com>
2023-10-25 11:35:47 +0300

    saving bookmarks to localStorage

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+const bookmarks = [];
+
 function showModal() {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
@@ -35,11 +37,19 @@ function validate(nameValue, urlValue) {
     }
     return true;
 }
+function fetchBookmarks() {
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+}
 
 function storeBookmark(e) {
     e.preventDefault();
     const nameValue = websiteNameEl.value;
     let urlValue = websiteUrlEl.value;
+
     if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
         urlValue = `https://${urlValue}`;
     }
@@ -47,6 +57,17 @@ function storeBookmark(e) {
     if (!validate(nameValue, urlValue)) {
         return;
     }
+
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+fetchBookmarks();
